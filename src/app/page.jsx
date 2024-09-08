28c78f384472253998bc2a2572a5b532a95bde19
Nigel Marshal <nigelmarshal@gmail.com>
2024-09-08 16:38:49 +0400

    style(home+about): Add styles and images

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,30 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Example Agency</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione
+          sequi, eveniet voluptatem expedita itaque nobis doloribus
+          reprehenderit non nemo consequatur. Exercitationem harum inventore
+          officia reprehenderit voluptate commodi ratione rerum quas!
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact Us</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,15 @@
+import styles from "./footer.module.css";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.logo}>NM</div>
+        <div className={styles.text}>
+          Example Agency &copy; All rights reserved
+        </div>
+      </div>
+    </>
+  );
 };
 export default Footer;
