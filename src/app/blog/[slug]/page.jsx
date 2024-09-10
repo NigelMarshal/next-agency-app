9522a5b0945acca3552bdaf071fcb7ef1d952287
Nigel Marshal <nigelmarshal@gmail.com>
2024-09-10 12:45:57 +0400

    feat(db): Connect app with mongodb

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -2,51 +2,34 @@ import Image from "next/image";
 import styles from "./singlePost.module.css";
 import PostUser from "@/components/postUser/postUser";
 import { Suspense } from "react";
-
-const getData = async (slug) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
-
-  if (!res.ok) {
-    throw new Error("Something went wrong");
-  }
-
-  const data = await res.json();
-  return data;
-};
+import { getPost } from "@/lib/data";
 
 const SinglePostPage = async ({ params }) => {
   const { slug } = params;
-  const post = await getData(slug);
+  const post = await getPost(slug);
 
   return (
     <div className={styles.container}>
-      <div className={styles.imgContainer}>
-        <Image
-          src="https://images.pexels.com/photos/1391498/pexels-photo-1391498.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""
-          fill
-          className={styles.img}
-        />
-      </div>
+      {post.img && (
+        <div className={styles.imgContainer}>
+          <Image src={post.img} alt="" fill className={styles.img} />
+        </div>
+      )}
+
       <div className={styles.textContainer}>
         <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
-          <Image
-            className={styles.avatar}
-            src="https://images.pexels.com/photos/609549/pexels-photo-609549.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            width={50}
-            height={50}
-          />
           <Suspense fallback={<div>Loading...</div>}>
             <PostUser userId={post.userId} />
           </Suspense>
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
-            <span className={styles.detailValue}>02/02/2022</span>
+            <span className={styles.detailValue}>
+              {post.createdAt.toString().slice(0, 16)}
+            </span>
           </div>
         </div>
-        <div className={styles.content}>{post.body}</div>
+        <div className={styles.content}>{post.desc}</div>
       </div>
     </div>
   );
---
+++ src/lib/data.js
@@ -0,0 +1,46 @@
+import { Post, User } from "./models";
+import { connectToDb } from "./utils";
+
+export const getPosts = async () => {
+  try {
+    connectToDb();
+    const posts = await Post.find();
+    return posts;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Error getting posts");
+  }
+};
+
+export const getPost = async (slug) => {
+  try {
+    connectToDb();
+    const post = await Post.findOne({ slug });
+    return post;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Error getting post");
+  }
+};
+
+export const getUser = async (id) => {
+  try {
+    connectToDb();
+    const user = await User.findById(id);
+    return user;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Error getting user");
+  }
+};
+
+export const getUsers = async () => {
+  try {
+    connectToDb();
+    const users = await User.find();
+    return users;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Error getting users");
+  }
+};
